df931e85c56b72fd4471b94c2dc4049a45a1485c
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-10-03 13:08:04 -0600

    pg 59 javascript assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+var x="variable is a \"string\"" // establish first variable used "\" to show qutation marks
+var y=" something else" // second varible string
+var x = x.fontcolor("blue") // makes first string varible the color blue
+document.write(x+y); // expression concenating two strings using document write
+window.alert("Welcome to the thunderdome"); // use of window.alert
