9105db70f012a1b2291a93ec9e5225daac79c2d9
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-10-07 08:56:06 -0600

    assignments for page 64

--- Basic JavaScript Projects/Project2_Functions/JS/Main.js
+++ Basic JavaScript Projects/Project2_Functions/JS/Main.js
@@ -1,15 +1,22 @@
-function myFirstFunc() {
+function myFirstFunc() { //function used just for an example
     var str = "this is green text!";
     var result = str.fontcolor("green");
     document.getElementById("Green_Text").innerHTML = result;
 }
 myFirstFunc();
-// add math expression here 
 
-function myMathFunc() {
+
+function myMathFunc() {// this is a math expression
     var num1 = 5;
     var num2 = 5;
     var answer = (num1 * num2);
     document.getElementById("answerReturn").innerHTML = answer;
 }
 myMathFunc()
+
+function myFunction() { //function that concatenates two string using += operator
+    var sentence = "I am Learning";
+    sentence += " a lot from this book";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
+myFunction()
