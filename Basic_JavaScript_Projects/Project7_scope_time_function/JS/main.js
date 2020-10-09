a4198854728bbb75c9cdba5e34b16c414104e995
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-10-09 10:13:11 -0600

    pg 142 assignment

--- Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -1,9 +1,22 @@
-function myNumFunc() {
-    if (document.getElementById("num") < 25) {
-    document.getElementById("response").innerHTML = "Add more units please";
+function Time_Function() { // this is the time function from the previous pages
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) { // if else statments
+        Reply = "It's morning time!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is the afternoon";
     }
     else {
-        document.getElementById("response").innerHTML = "It's too big now... sad";
+        Reply = "It's the evening time";
     }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+    }
+
+var global = new Date(); // global variable
+function fullYear() {
+    var year = (global.getFullYear() + " is the year!"); // local variable this is also where i used the console to debug 
+    document.getElementById("dateYear").innerHTML = year; // and issue saying the variable was null
 }
 
+fullYear();
