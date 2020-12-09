83a171243695957675220c0b88849700a1e0f716
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-12-09 12:51:51 -0700

    Organization update

--- Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -1,24 +1,14 @@
-function myConcat() { //use of the concat function displays in html
-    var part1 = "the concat ";
-    var part2 = "is putting these ";
-    var part3 = "strings ";
-    var part4 = "together.";
-    var sentence = part1.concat(part2, part3, part4);
-    document.getElementById("string").innerHTML = sentence;
-}
+// NEW Code 
+function countdown() { // initial input of seconds by user
+    var seconds = document.getElementById("seconds").value;
 
-function mySlice() { // use of the slice function with two parameters displays in the console
-    var X = "this will be the victim of the slicing.";
-    var Y = X.slice(4,15);
-    console.log(Y);
+    function tick() { // counting down function
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+    if(seconds == -1){ 
+        alert("Time's up!"); // end of counting function
+    }
+        }
+        tick(); // calls counting function
 }
-
-function myDouble() { 
-    var P = 5439/95.5;
-    console.log(P);
-    var G = P.toPrecision(4);
-    var V = G.toString();
-    document.getElementById("stringer").innerHTML = V;
-}
-
-myDouble();
